4b58efb6499450dc1c0da2f8d6e480d864d340a2
mallia01 <mallick.abhinit@gmail.com>
2018-06-04 00:45:15 +0530

    Added download of README in /tmp from nexus using wget http://localhost:8081/nexus/service/local/repositories/hello_world_nexus_repo/content/README

--- create_file.sh
+++ create_file.sh
@@ -9,3 +9,8 @@ echo "###########"
 echo "Current time"
 date
 echo "###########"
+cd /tmp;
+wget http://localhost:8081/nexus/service/local/repositories/hello_world_nexus_repo/content/README
+cat /tmp/README;
+echo "###########"
+
