e3cdfffafa9be4795dff734cfb317d719773a40b
mallia01 <mallick.abhinit@gmail.com>
2018-06-03 19:25:50 +0530

    This repo is used to create hello_world.html in the /tmp Once this fine is created it will have the date followed by the tag created by mallia01

---
+++ create_file.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+>/tmp/hello_world.html
+echo date" created by mallia01" >> /tmp/hello_world.html
+echo "###########"
+ls -l /tmp/hello_world.html
+echo "###########"
+cat /tmp/hello_world.html
+echo "###########"
+echo "Current time"
+date
